672650fdc4e9a43b867d8d070c1caf7b6eb70501
ThickDepression <yami.onlyme@gmail.com>
2023-03-17 18:53:53 +0100

    his program will assign a random number to the variable n each time it is executed.

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,15 +13,15 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 	{
-		printf(n" is positive");
+		printf("%d is positive", n);
 	}
 	else if (n < 0)
 	{
-		printf(n" is negative");
+		printf("%d is negative", n);
 	}
 	else
 	{
-		printf(n" is zerp");
+		printf("%d is zerp", n);
 	}
 	return (0);
 }
