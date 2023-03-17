f7b0e41b9a4329843263f7e02e0cd0f6774d1599
ThickDepression <yami.onlyme@gmail.com>
2023-03-17 01:11:29 +0100

    a C program that prints

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - Entry point
  *
  * Return: Always 1 (Success)
  */
-
 int main(void)
 {
-write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, str, sizeof(str) - 1);
 return (1);
 }
+
