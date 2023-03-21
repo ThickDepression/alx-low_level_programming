ebb6e2a374f4714c6b8d456914943df3cd62cc92
ThickDepression <yami.onlyme@gmail.com>
2023-03-21 13:06:12 +0000

    a function that prints the alphabet, in lowercase, followed by a new line.

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,3 +11,46 @@ int print(char *array, int length)
 {
          return (write(1, &array[0], length));
 }
+
+
+
+
+
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+
+
+
+#include <unistd.h>
+
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void print_alphabet(void)
+{
+  char alphabets[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+  int i = 0;
+  while (i < 26)
+    {
+      write(1, &alphabets[i], 1);
+      ++i;
+    }
+}
