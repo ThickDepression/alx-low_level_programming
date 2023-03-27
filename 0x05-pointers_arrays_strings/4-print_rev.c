c04d8de7630575a78f58b1a4e22e0d69ed7a0115
ThickDepression <yami.onlyme@gmail.com>
2023-03-27 06:46:39 +0000

    task4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,22 +1,28 @@
 #include "main.h"
 
 /**
- *_puts - type
+ *print_rev - types in rev
  *@s: variable
  *Return: Always 0.
  */
 
 void print_rev(char *s)
 {
-  int i = _strlen(s);
-  int a;
+int i = _strlen(s);
+int a;
 
-  for (a = i - 1; a >= 0; a--)
-    {
-      _putchar(s[a]);
-    }
-  _putchar('\n');
+for (a = i - 1; a >= 0; a--)
+{
+_putchar(s[a]);
 }
+_putchar('\n');
+}
+
+/**
+ *_strlen - counts characters
+ *@s: value
+ *Return: counter
+ */
 
 int _strlen(char *s)
 {
