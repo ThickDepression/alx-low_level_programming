f971f3ed14f06c319452fc9f28302128aaa5f602
ThickDepression <yami.onlyme@gmail.com>
2023-03-27 17:00:18 +0000

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ *puts2 - do smtg
+ *@str: string
+ *Return: returns
+ */
+
+void puts2(char *str)
+{
+int i = _strlen(str);
+int a = 0;
+do {
+if (a == 0 || a % 2 == 0)
+{
+_putchar(str[a]);
+a++;
+}
+else
+{
+a++;
+}
+} while (a < i);
+_putchar('\n');
+}
+
+
+/**
+ *_strlen - counts characters
+ *@s: value
+ *Return: counter
+ */
+
+int _strlen(char *s)
+{
+int counter = 0;
+while (*s != '\0')
+{
+counter++;
+s++;
+}
+return (counter);
+}
