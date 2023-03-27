8caab539b027833d17ee23685affc7deb1f36a46
ThickDepression <yami.onlyme@gmail.com>
2023-03-27 18:03:13 +0000

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,10 +9,36 @@
 void puts2(char *str)
 {
 int i;
+int _break = _strlen(str);
 
-for (i = 0; str[i] != '\0'; i += 2)
+for (i = 0; i < _break; i += 2)
+{
+if (str[i] != '\0')
 {
 _putchar(str[i]);
 }
+else
+{
+i = _break;
+}
+}
 _putchar('\n');
+}
+
+
+/**
+ *_strlen - counts characters
+ *@s: value
+ *Return: counter
+ */
+
+int _strlen(char *s)
+{
+int counter = 0;
+while (*s != '\0')
+{
+counter++;
+s++;
+}
+return (counter);
 }
