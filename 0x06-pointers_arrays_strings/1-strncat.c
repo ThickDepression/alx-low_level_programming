72d259398b685323d9e11f72404dcb9b34479d09
ThickDepression <yami.onlyme@gmail.com>
2023-03-29 20:05:09 +0000

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,19 +9,18 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-int i, j;
-
-i = 0;
+int i = 0;
+int j = 0;
 while (dest[i] != '\0')
 {
 i++;
 }
-j = 0;
-while (j < n)
+while (j < n && src[j] != '\0')
 {
 dest[i] = src[j];
 i++;
 j++;
 }
+dest[i] = '\0';
 return (dest);
 }
