4bc81f476cc877b3201fd96998dc4c0057246c46
ThickDepression <yami.onlyme@gmail.com>
2023-03-30 16:59:14 +0000

    task 6

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *_strcmp - compare
+ *@s1: string 1
+ *@s2: string 2
+ *Return: returns
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+int char1, char2;
+char1 = char2 = 0;
+if (s1[char1] == s2[char2])
+{
+if (s1[char1] != '\0' || s2[char2] != '\0')
+{
+char1++;
+char2++;
+}
+else
+{
+return (0);
+}
+}
+else
+{
+if (s1[char1] < s2[char2])
+return (-15);
+if (s1[char1] > s2[char2])
+return (15);
+}
+return (0);
+}
