5f7c896ab879f875974c1f9c5d8a64cccfbf1b12
ThickDepression <yami.onlyme@gmail.com>
2023-04-03 19:51:39 +0000

    task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *_strstr - a function that locates a substring.
+ *@haystack: the main string
+ *@needle: the substring
+ *Return: return a pointer
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+char *a = needle;
+while (*a)
+{
+while (*haystack)
+{
+if (*a == *haystack)
+{
+return (haystack);
+}
+haystack++;
+}
+a++;
+}
+return ('\0');
+}
