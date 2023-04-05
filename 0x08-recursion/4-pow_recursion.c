4efad607bfc1c2692e370df57fb2f7c5028f4a8c
ThickDepression <yami.onlyme@gmail.com>
2023-04-05 16:41:28 +0000

    task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *_pow_recursion - a function that returns the value of pow.
+ *@x: num
+ *@y: pow
+ *Return: return m
+ */
+
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+else if (y > 0)
+{
+return (x * _pow_recursion(x, y - 1));
+}
+if (y == 1)
+{
+return (x);
+}
+}
