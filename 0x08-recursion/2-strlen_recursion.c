df8cfd82f187830719156627ca818400ac7aaa04
ThickDepression <yami.onlyme@gmail.com>
2023-04-05 16:20:12 +0000

    task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,9 +8,13 @@
 
 int _strlen_recursion(char *s)
 {
-  if (!*(s+1))
-  {
-    return (1);
-  }
-  return (_strlen_recursion(s + 1) + 1);
+if (!*s)
+{
+return (0);
+}
+if (!*(s+1))
+{
+return (1);
+}
+return (_strlen_recursion(s + 1) + 1);
 }
