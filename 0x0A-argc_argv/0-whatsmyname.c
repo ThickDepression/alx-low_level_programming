1511be5d95172554556e93cc1a2f18a64246156b
ThickDepression <yami.onlyme@gmail.com>
2023-04-08 15:03:09 +0000

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,11 +7,7 @@
  *Return: returns 0
 */
 
-int main(int argc, char *argv[])
-{
-if (argc > 0)
+int main(int argc __attribute__((unused)), char *argv[])
 {
 return (printf("%s\n", argv[0]));
-}
-return (0);
 }
