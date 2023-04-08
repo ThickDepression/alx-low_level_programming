e99daa5293cefbff582b3230ce5ed79eca9d06b3
ThickDepression <yami.onlyme@gmail.com>
2023-04-08 16:05:16 +0000

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+
+int _atoi(char *s)
+{
+int i, d, n, len, f, digit;
+i = 0;
+d = 0;
+n = 0;
+len = 0;
+f = 0;
+digit = 0;
+while (s[len] != '\0')
+len++;
+while (i < len && f == 0)
+{
+if (s[i] == '-')
+++d;
+if (s[i] >= '0' && s[i] <= '9')
+{
+digit = s[i] - '0';
+if (d % 2)
+digit = -digit;
+n = n * 10 + digit;
+f = 1;
+if (s[i + 1] < '0' || s[i + 1] > '9')
+break;
+f = 0;
+}
+i++;
+}
+if (f == 0)
+return (0);
+return (n);
+}
+
+/**
+ *main - main function
+ *@argc: arg count
+ *@argv: arg vector
+ *Return: 0 for success
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+if (!argv[0])
+{
+printf("Error");
+return (1);
+}
+else
+{
+printf("%d\n", _atoi(argv[1]) * _atoi(argv[2]));
+return (0);
+}
+}
