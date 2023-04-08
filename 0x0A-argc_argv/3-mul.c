ef195ac7f8b25e1ab3309f90c7d816d527b85e79
ThickDepression <yami.onlyme@gmail.com>
2023-04-08 19:50:42 +0000

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,14 +13,14 @@ int main(int argc, char *argv[])
 int num1 = atoi(argv[1]);
 int num2 = atoi(argv[2]);
 int result = num1 * num2;
-if (argc == 3)
-{
-printf("%d\n", result);
-return (0);
-}
-else
+if (argc > 3 || argc < 3)
 {
 printf("Error\n");
 return (1);
+}
+else
+{
+printf("%d\n", result);
+return (0);
 }
 }
