1f3466e7bc6953201f071a7b1e3ce1da650ab300
ThickDepression <yami.onlyme@gmail.com>
2023-04-09 18:04:24 +0000

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,20 +11,21 @@
 
 int main(int argc, char *argv[])
 {
-int i;
-int result = 0;
-for (i = 1; i < argc; i++)
-{
-if (isdigit(*argv[i]))
-{
-result += atoi(argv[i]);
-}
-else
-{
-printf("Error\n");
-return (1);
-}
-}
-printf("%d\n", result);
-return (0);
+	int i;
+	int result = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (isdigit(*argv[i]))
+		{
+			result += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", result);
+	return (0);
 }
