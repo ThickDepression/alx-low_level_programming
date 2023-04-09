1d48fa942ebb2774824db0e776d8f8ba0ddd4bcd
ThickDepression <yami.onlyme@gmail.com>
2023-04-09 20:10:04 +0000

    task 5

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int centfinder(int n);
+/**
+ *main - main function
+ *@argc: arg count
+ *@argv: arg vector
+ *Return: 0 for success
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+		if (atoi(argv[1]) < 0)
+		{
+		printf("0\n");
+		}
+		printf("%d\n", centfinder(atoi(argv[1])));
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
+/**
+ *centfinder - finds the min of change
+ *@n: num passed from main
+ *Return: returns the count
+ */
+
+int centfinder(int n)
+{
+	int count = 0;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	if (n >= 25)
+	{
+		count++;
+		count += centfinder(n - 25);
+	}
+	else if (n >= 10)
+	{
+		count++;
+		count += centfinder(n - 10);
+	}
+	else if (n >= 5)
+	{
+		count++;
+		count += centfinder(n - 5);
+	}
+	else if (n >= 2)
+	{
+		count++;
+		count += centfinder(n - 2);
+	}
+	else if (n >= 1)
+	{
+		count++;
+		count += centfinder(n - 1);
+	}
+	return (count);
+}
