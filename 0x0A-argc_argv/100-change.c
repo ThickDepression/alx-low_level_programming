e9945f134541d113f18a541ac3ac97877fa7dded
ThickDepression <yami.onlyme@gmail.com>
2023-04-10 03:39:36 +0000

    task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int coinCounter(int n);
+int coin_counter(int n);
 /**
  *main - main function
  *@argc: arg count
@@ -18,15 +18,15 @@ int main(int argc, char *argv[])
 	{
 		return (printf("0\n"), 0);
 	}
-	printf("%d\n", coinCounter(atoi(argv[1])));
+	printf("%d\n", coin_counter(atoi(argv[1])));
 	return (0);
 }
 /**
- *coinCounter - finds the min of change
+ *coin_counter - finds the min of change
  *@n: num passed from main
  *Return: returns the count
  */
-int coinCounter(int n)
+int coin_counter(int n)
 {
 	int count = 0;
 
@@ -36,28 +36,19 @@ int coinCounter(int n)
 	}
 	if (n >= 25)
 	{
-		count++;
-		count += coinCounter(n - 25);
+		count += 1 + coin_counter(n - 25);
 	}
 	else if (n >= 10)
 	{
-		count++;
-		count += coinCounter(n - 10);
+		count += 1 + coin_counter(n - 10);
 	}
 	else if (n >= 5)
 	{
-		count++;
-		count += coinCounter(n - 5);
-	}
-	else if (n >= 2)
-	{
-		count++;
-		count += coinCounter(n - 2);
+		count += 1 + coin_counter(n - 5);
 	}
 	else if (n >= 1)
 	{
-		count++;
-		count += coinCounter(n - 1);
+		count += 1 + coin_counter(n - 1);
 	}
 	return (count);
 }
