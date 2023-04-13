90269caf39224e09306568eba02735b95a87363c
ThickDepression <yami.onlyme@gmail.com>
2023-04-13 15:51:28 +0000

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,7 @@ int _strlen(char *s)
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
-	unsigned int i, j;
+	unsigned int i;
 	unsigned int len1 = _strlen(s1);
 	unsigned int len2 = _strlen(s2);
 
@@ -53,14 +53,17 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		return (NULL);
 	}
-	for (i = 0; s1[i]; i++)
+	for (i = 0; len1 + n; i++)
 	{
-		ptr[i] = s1[i];
+		if (i < len1)
+		{
+			ptr[i] = s1[i];
+		}
+		else
+		{
+			ptr[i] = s2[i - len1];
+		}
 	}
-	for (j = 0; j < n; j++)
-	{
-		ptr[i + j] = s2[j];
-	}
-	ptr[i + j] = '\0';
+	ptr[i] = '\0';
 	return (ptr);
 }
