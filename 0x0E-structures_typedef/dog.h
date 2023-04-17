a29d2d7f87a7640c3dadd7ca813e6ac323930a82
ThickDepression <yami.onlyme@gmail.com>
2023-04-17 16:10:10 +0000

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - structure of a dog
+ *@name: name of the dog
+ *@age: his age
+ *@owner: his owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
