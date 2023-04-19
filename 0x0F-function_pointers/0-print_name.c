588295d0f371174c34d1e30e0856d2adf3183ff4
ThickDepression <yami.onlyme@gmail.com>
2023-04-19 13:04:22 +0000

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ *print_name - a function that prints a name.
+ *@name: the name to be printed
+ *@f: the function used to print
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == &print_name_as_is)
+		print_name_as_is(name);
+	if (f == &print_name_uppercase)
+		print_name_uppercase(name);
+}
