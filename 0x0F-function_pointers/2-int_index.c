3a95a53f9d093abfc72fe4099a0a568c0d84129b
ThickDepression <yami.onlyme@gmail.com>
2023-04-19 14:49:38 +0000

    task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ *int_index -  a function that searches for an integer.
+ *@array: array searched in
+ *@size: number of elements in that array
+ *@cmp; a pointer to the usable function
+ *Return: 0 for success
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		return (1);
+	for (i = 0; i < size; i++)
+	{
+	if (cmp(array[i]))
+		return (i);
+	}
+	return (0);
+}
