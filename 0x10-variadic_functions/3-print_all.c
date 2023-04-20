39a3a6d38b8cf2ee8784e6883191cba4a53420c1
ThickDepression <yami.onlyme@gmail.com>
2023-04-20 18:49:22 +0000

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,31 +7,33 @@
  */
 void print_args(const char *format, va_list args)
 {
-	char *str, *sep = "";
+	char *str, *sep = ", ";
 	int x = 0;
 
 
 	while (format != NULL && format[x] != '\0')
 	{
+		if (format[x + 1] == '\0')
+			sep = "";
 		switch (format[x])
 		{
 			case 'c':
-				printf("%s%c", sep, va_arg(args, int));
+				printf("%c%s", va_arg(args, int), sep);
 				break;
 			case 'i':
-				printf("%s%d", sep, va_arg(args, int));
+				printf("%d%s", va_arg(args, int), sep);
 				break;
 			case 'f':
-				printf("%s%f", sep, va_arg(args, double));
+				printf("%f%s", va_arg(args, double), sep);
 				break;
 			case 's':
 				str = va_arg(args, char *);
 				if (str == NULL)
 				{
-					printf("%s(nil)", sep);
+					printf("(nil)%s", sep);
 					break;
 				}
-				printf("%s%s", sep, str);
+				printf("%s%s", str, sep);
 				break;
 			default:
 				break;
