131a3aca65a11de50cdae8f0223eb8363fb79910
ThickDepression <yami.onlyme@gmail.com>
2023-04-27 21:41:12 +0000

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ *fasterthanlight - a function that prints before main
+ *
+ *
+ */
+
+void fasterthanlight(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
