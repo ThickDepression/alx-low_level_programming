84ad6c0f33addc092c551bf3f7e90adb349b4658
ThickDepression <yami.onlyme@gmail.com>
2023-04-27 08:37:45 +0000

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ *print_list - a function that prints all the elements of a list.
+ *@h: list
+ *
+ *Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	int nodes_num = 0;
+
+	while (h)
+	{
+		if (h == NULL)
+			printf("[0] (nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		nodes_num++;
+	}
+	return (nodes_num);
+}
