9a5dac9b15f7ca79c1a4d01c1e6614cf9fbb67f2
ThickDepression <yami.onlyme@gmail.com>
2023-04-30 14:35:18 +0100

    task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ *pop_listint -  function that deletes the head node
+ *@head: head node
+ *Return: 0
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int num;
+
+	if (!*head)
+		return (0);
+	temp = *head;
+	*head = (*head)->next;
+	num = temp->n;
+	return (num);
+}
