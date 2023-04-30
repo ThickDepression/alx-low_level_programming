3caa15f63f38f4b287d89e8b02ecc3f3d3ca89d1
ThickDepression <yami.onlyme@gmail.com>
2023-04-30 15:03:42 +0100

    task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,23 +1,6 @@
 #include "lists.h"
 
 /**
- *node_counter - count nodes obv
- *@head: node
- *Return: count
- */
-int node_counter(listint_t *head)
-{
-	int counter = 0;
-
-	while (head)
-	{
-		head = head->next;
-		counter++;
-	}
-	return (counter);
-}
-
-/**
  *get_nodeint_at_index - returns nth node
  *@index: is the index of the node
  *@head: node
@@ -26,22 +9,16 @@ int node_counter(listint_t *head)
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *node;
-	int ind = index;
-	int max = node_counter(head);
-	int count = 0;
+	listint_t *node = head; 
+	unsigned int count = 0;
 
-	if (ind < 0 || ind > max)
-		return (NULL);
-	while (head)
+
+	while (node && count < index)
 	{
-		head = head->next;
+		node = node->next;
 		count++;
-		if (ind == count)
-		{
-			node = head;
-			break;
-		}
 	}
+	if (count != index || node == NULL)
+		return (NULL);
 	return (node);
 }
