b9ee54f212cb76bd1f552f036eb1f5d776fd7aed
ThickDepression <yami.onlyme@gmail.com>
2023-04-30 14:15:37 +0100

    task5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ *free_listint2 - function that frees
+ *@head: pointer
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *crnt = *head, *temp;
+
+	while (crnt)
+	{
+		temp = crnt->next;
+		free(crnt);
+		crnt = temp;
+	}
+	*head =  NULL;
+}
