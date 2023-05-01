d92603db7eb934c1818a9ea5c8e2157d3a2bf27a
ThickDepression <yami.onlyme@gmail.com>
2023-05-01 16:09:46 +0100

    task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -16,19 +16,20 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 	current = *head;
 	previous = NULL;
+	if (!previous)
+	{
+		*head = current->next;
+		free(current);
+		return (1);
+	}
 	for (count = 0; !current && count < index; count++)
 	{
 		previous = current;
 		current = current->next;
 	}
 	if (!current)
-	{
 		return (-1);
-	}
-	if (!previous)
-		*head = current->next;
-	else
-		previous->next = current->next;
+	previous->next = current->next;
 	free(current);
 	return (1);
 }
