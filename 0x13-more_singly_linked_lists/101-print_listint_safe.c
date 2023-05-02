155b7a792c6f6463cc13493b972bc67e4e8257bd
ThickDepression <yami.onlyme@gmail.com>
2023-05-02 13:22:44 +0100

    task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,40 +1,6 @@
 #include "lists.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- *num_printer - prints nums
- *@n: num
- */
-
-void num_printer(int n)
-{
-	unsigned int num;
-
-	num = n;
-	if (n < 0)
-	{
-		_putchar(45);
-		num = -n;
-	}
-	if (num / 10)
-	{
-		num_printer(num / 10);
-	}
-	_putchar((num % 10) + '0');
-}
-
-/**
  *print_listint_safe - prints the list
  *@head: node
  *Return: returns num of nodes
@@ -42,16 +8,20 @@ void num_printer(int n)
 
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t count = 0;
+    size_t count = 0;
+    const listint_t *temp;
 
-	while (head)
-	{
-		if (head->n == '\0')
-			exit(98);
-		num_printer(head->n);
-		_putchar('\n');
-		head = head->next;
-		count++;
-	}
-	return (count);
+    while (head != NULL)
+    {
+        printf("[%p] %d\n", (void *)head, head->n);
+        count++;
+        temp = head;
+        head = head->next;
+        if (temp <= head)
+        {
+            printf("-> [%p] %d\n", (void *)head, head->n);
+            exit(98);
+        }
+    }
+    return (count);
 }
