c69fe7f631810c95e15d24b2ac548354ffea8534
ThickDepression <yami.onlyme@gmail.com>
2023-05-02 12:56:11 +0100

    task 12

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 #define LISTS_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 /**
  * struct listint_s - singly linked list
@@ -17,6 +18,7 @@ typedef struct listint_s
 } listint_t;
 
 int _putchar(char c);
+void num_printer(int n);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
