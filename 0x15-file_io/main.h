04b5daca834b3d7c761e2bac3d03d5825d6ffda3
ThickDepression <yami.onlyme@gmail.com>
2023-05-09 00:33:22 +0100

    task 3

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,9 +8,10 @@
 
 
 int _putchar(char c);
+int _strlen(char *s);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
+void textfile_copier(int cpied_fl, int fl_cp, char *first_fl, char *snd_fl);
 
 #endif
