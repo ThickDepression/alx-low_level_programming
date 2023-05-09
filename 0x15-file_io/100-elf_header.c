3725a441deb4a1a1c3f4f3c5a39d63acf901252f
ThickDepression <yami.onlyme@gmail.com>
2023-05-09 01:35:17 +0100

    task 4

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,89 +1,62 @@
 #include "main.h"
 
-/**
- *_memcmp - a function that compares tfco blocks of memory.
- *@s1:  pointer to the memory block
- *@s2:  pointer to the memory block
- *@n: the number of bytes to compare.
- *Return: 0
- **/
 
-int _memcmp(const void *s1, const void *s2, size_t n)
+int main(int argc, char *argv[])
 {
-	const unsigned char *p1 = s1, *p2 = s2;
-	size_t i;
+	int fd, n;
+	Elf64_Ehdr eh;
 
-	for (i = 0; i < n; i++)
+	if (argc != 2)
 	{
-		if (p1[i] != p2[i])
-			return ((p1[i] < p2[i]) ? -1 : 1);
+		fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
+		exit(98);
 	}
-	return (0);
-}
-
-/**
- *error_printer - prints error
- *@msg: error msg
- */
-
-void error_printer(char *msg)
-{
-	dprintf(STDERR_FILENO, "Error: %s\n", msg);
-	exit(98);
-}
-
-/**
- *elf_header_printer - prints the header
- *@header: header
- */
-
-void elf_header_printer(Elf64_Ehdr *header)
-{
-	int i;
-
-	printf("ELF Header:\n");
-	printf("  Magic:   ");
-	for (i = 0; i < EI_NIDENT; i++)
-	{
-		printf("%02x ", header->e_ident[i]);
-	}
-	printf("\n");
-	printf("  Class:                             %s\n", header->e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
-	printf("  Data:                              %s\n", header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
-	printf("  Version:                           %d%s\n", header->e_ident[EI_VERSION], header->e_ident[EI_VERSION] == EV_CURRENT ? " (current)" : "");
-	printf("  OS/ABI:                            %s\n", header->e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" : "Others");
-	printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
-	printf("  Type:                              %s\n", header->e_type == ET_EXEC ? "EXEC (Executable file)" : "OTHER (Unknofcn file type)");
-	printf("  Entry point address:               0x%lx\n", header->e_entry);
-	printf("\n");
-}
-
-/**
- *main - main function
- *@ac: argument count
- *@av: arguments
- *Return: 0
- */
-
-int main(int ac, char *av[])
-{
-	int fd, fr, fc;
-	Elf64_Ehdr header;
-
-	if (ac != 2)
-	{
-		error_printer("Invalid number of arguments.");
-	}
-	fd = open(av[1], O_RDONLY);
+	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
-		error_printer("Cannot open file");
-	fr = read(fd, &header, sizeof(header));
-	if (fr == -1)
-		error_printer("Cannot read file header");
-	fc = _memcmp(header.e_ident, ELFMAG, SELFMAG);
-	if (fc != 0)
-		error_printer("Not an ELF file");
-	elf_header_printer(&header);
+	{
+		fprintf(stderr, "Error: cannot open %s\n", argv[1]);
+		exit(98);
+	}
+	n = read(fd, &eh, sizeof(eh));
+	if (n != sizeof(eh))
+	{
+		fprintf(stderr, "Error: cannot read ELF header\n");
+		exit(98);
+	}
+	if (eh.e_ident[EI_MAG0] != ELFMAG0 || eh.e_ident[EI_MAG1] != ELFMAG1 ||
+	eh.e_ident[EI_MAG2] != ELFMAG2 || eh.e_ident[EI_MAG3] != ELFMAG3)
+	{
+		fprintf(stderr, "Error: %s is not an ELF file\n", argv[1]);
+		exit(98);
+	}
+	printf("ELF Header:\n");
+	printf("  Magic:   %02x %02x %02x %02x\n",
+	eh.e_ident[EI_MAG0], eh.e_ident[EI_MAG1],
+	eh.e_ident[EI_MAG2], eh.e_ident[EI_MAG3]);
+	printf("  Class:                             %s\n",
+	eh.e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" :
+	(eh.e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "Unknown"));
+	printf("  Data:                              %s\n",
+	eh.e_ident[EI_DATA] == ELFDATA2LSB ?
+	"2's complement, little endian" :
+	(eh.e_ident[EI_DATA] == ELFDATA2MSB ?
+	"2's complement, big endian" : "Unknown"));
+	printf("  Version:                           %d%s\n",
+	eh.e_ident[EI_VERSION],
+	eh.e_ident[EI_VERSION] == EV_CURRENT ? " (current)" : "");
+	printf("  OS/ABI:                            %s\n",
+	eh.e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" :
+	(eh.e_ident[EI_OSABI] == ELFOSABI_LINUX ? "UNIX - Linux" : "Unknown"));
+	printf("  ABI Version:                       %d\n",
+	eh.e_ident[EI_ABIVERSION]);
+	printf("  Type:                              %s\n",
+	eh.e_type == ET_REL ? "REL (Relocatable file)" :
+	(eh.e_type == ET_EXEC ? "EXEC (Executable file)" :
+	(eh.e_type == ET_DYN ? "DYN (Shared object file)" :
+	(eh.e_type == ET_CORE ? "CORE (Core file)" : "Unknown"))));
+	printf("  Entry point address:               0x%lx\n",
+	(unsigned long)eh.e_entry);
+
 	close(fd);
 	return (0);
 }
