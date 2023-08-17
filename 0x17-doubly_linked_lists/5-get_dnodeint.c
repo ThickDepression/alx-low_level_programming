ee7801be736e9b767d5ac3db5caa7bb8d639a464
ThickDepression <yami.onlyme@gmail.com>
2023-08-17 18:16:54 +0100

    add

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ *get_dnodeint_at_index - that returns the nth node of a linked list.
+ *@head: head of the list
+ *@index: index of the node
+ *Return: returns the nth node
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *node;
+	unsigned int idx = 0;
+
+	if (!head)
+		return (NULL);
+	node = malloc(sizeof(dlistint_t));
+	while (idx <= index)
+	{
+		if (idx == index)
+			node = head;
+		head = head->next;
+		idx++;
+	}
+	return (node);
+}
