9fc29a39dd12e8b5a5a1b554947175616621457c
ThickDepression <yami.onlyme@gmail.com>
2023-08-18 00:02:24 +0100

    add

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,40 +10,41 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new, *temp;
+	dlistint_t *new;
+	dlistint_t *head;
 	unsigned int i;
 
 	new = NULL;
 	if (idx == 0)
 		new = add_dnodeint(h, n);
 	else
-	{
-		temp = *h;
+		{
+		head = *h;
 		i = 1;
-		if (temp)
-			while (temp->prev)
-				temp = temp->prev;
-		while (temp)
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+		while (head != NULL)
 		{
 			if (i == idx)
 			{
-				if (!temp->next)
+				if (head->next == NULL)
 					new = add_dnodeint_end(h, n);
-				else
-				{
+					else
+					{
 					new = malloc(sizeof(dlistint_t));
-					if (new)
+				if (new != NULL)
 					{
 						new->n = n;
-						new->next = temp->next;
-						new->prev = temp;
-						temp->next->prev = new;
-						temp->next = new;
+						new->next = head->next;
+						new->prev = head;
+						head->next->prev = new;
+						head->next = new;
 					}
 				}
 				break;
 			}
-			temp = temp->next;
+			head = head->next;
 			i++;
 		}
 	}
