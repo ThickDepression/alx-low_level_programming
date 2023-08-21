11ae196f34cdd8e69927ce78008793e41fbf36d9
ThickDepression <yami.onlyme@gmail.com>
2023-08-21 16:27:07 +0100

    add

--- 0x18-dynamic_libraries/_puts.c
+++ 0x18-dynamic_libraries/_puts.c
@@ -1,17 +1,1 @@
-#include "main.h"
 
-/**
- *_puts - type
- *@str: variable
- *Return: Always 0.
- */
-
-void _puts(char *str)
-{
-	while (*str != '\0')
-	{
-		_putchar(*str);
-		str++;
-	}
-	_putchar('\n');
-}
