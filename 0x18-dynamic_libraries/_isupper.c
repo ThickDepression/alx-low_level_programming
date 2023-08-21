5d2396915497956c81d80f4e1354c99aeddd6088
ThickDepression <yami.onlyme@gmail.com>
2023-08-21 16:24:39 +0100

    add

--- 0x18-dynamic_libraries/_isupper.c
+++ 0x18-dynamic_libraries/_isupper.c
@@ -1,19 +1,1 @@
-#include "main.h"
 
-/**
- *_isupper - checks for uppers
- *@c: letter in question
- *Return: always something
- */
-
-int _isupper(int c)
-{
-	if (c >= 65 && c <= 90)
-	{
-		return (1);
-	}
-	else
-{
-		return (0);
-	}
-}
