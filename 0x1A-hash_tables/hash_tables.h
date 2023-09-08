6685d6cc4d1e210840736a1e4bbf0361ac63c381
ThickDepression <yami.onlyme@gmail.com>
2023-09-08 17:03:16 +0100

    betty for header

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -15,12 +15,13 @@
  * @sprev: A pointer to the previous element of the sorted linked list
  * @snext: A pointer to the next element of the sorted linked list
  */
-typedef struct shash_node_s {
-  char *key;
-  char *value;
-  struct shash_node_s *next;
-  struct shash_node_s *sprev;
-  struct shash_node_s *snext;
+typedef struct shash_node_s
+{
+	char *key;
+	char *value;
+	struct shash_node_s *next;
+	struct shash_node_s *sprev;
+	struct shash_node_s *snext;
 } shash_node_t;
 
 /**
@@ -33,11 +34,12 @@ typedef struct shash_node_s {
  * @shead: A pointer to the first element of the sorted linked list
  * @stail: A pointer to the last element of the sorted linked list
  */
-typedef struct shash_table_s {
-  unsigned long int size;
-  shash_node_t **array;
-  shash_node_t *shead;
-  shash_node_t *stail;
+typedef struct shash_table_s
+{
+	unsigned long int size;
+	shash_node_t **array;
+	shash_node_t *shead;
+	shash_node_t *stail;
 } shash_table_t;
 
 /**
@@ -48,10 +50,11 @@ typedef struct shash_table_s {
  * @value: The value corresponding to a key
  * @next: A pointer to the next node of the List
  */
-typedef struct hash_node_s {
-  char *key;
-  char *value;
-  struct hash_node_s *next;
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -62,9 +65,10 @@ typedef struct hash_node_s {
  * Each cell of this array is a pointer to the first node of a linked list,
  * because we want our HashTable to use a Chaining collision handling
  */
-typedef struct hash_table_s {
-  unsigned long int size;
-  hash_node_t **array;
+typedef struct hash_table_s
+{
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
 /* 0-hash_table_create.c */
@@ -79,7 +83,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size);
 /* 3-hash_table_set.c */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 int addnode(hash_node_t *node, hash_table_t *ht, unsigned long int idx,
-            const char *key, const char *value);
+						const char *key, const char *value);
 /* 4-hash_table_get.c */
 char *hash_table_get(const hash_table_t *ht, const char *key);
 
