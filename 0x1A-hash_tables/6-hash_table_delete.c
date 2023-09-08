a6f8dd2b6e54c3bbd40de72fcb9aca689b2cd000
ThickDepression <yami.onlyme@gmail.com>
2023-09-08 18:21:42 +0100

    task 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -2,22 +2,28 @@
 #include <stdlib.h>
 
 /**
- *hash_table_delete - deletes a hash table.
- *@ht: hash table
+ * hash_table_delete - Deletes a hash table and its elements.
+ * @ht: A pointer to the hash table.
  */
-
 void hash_table_delete(hash_table_t *ht) {
-  hash_node_t *tmp;
-  unsigned long int index = 0;
+  hash_node_t *current, *temp;
+  unsigned long int i = 0;
 
-  if (!ht)
+  if (ht == NULL)
     return;
 
-  for (; index < ht->size; index++) {
-    tmp = ht->array[index];
-    while (tmp) {
-      free(tmp);
-      tmp = ht->array[index]->next;
+  for (; i < ht->size; i++) {
+    current = ht->array[i];
+    while (current != NULL) {
+      temp = current;
+      current = current->next;
+      free(temp->key);
+      free(temp->value);
+      free(temp);
     }
+    free(current);
   }
+
+  free(ht->array);
+  free(ht);
 }
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -15,13 +15,12 @@
  * @sprev: A pointer to the previous element of the sorted linked list
  * @snext: A pointer to the next element of the sorted linked list
  */
-typedef struct shash_node_s
-{
-	char *key;
-	char *value;
-	struct shash_node_s *next;
-	struct shash_node_s *sprev;
-	struct shash_node_s *snext;
+typedef struct shash_node_s {
+  char *key;
+  char *value;
+  struct shash_node_s *next;
+  struct shash_node_s *sprev;
+  struct shash_node_s *snext;
 } shash_node_t;
 
 /**
@@ -34,12 +33,11 @@ typedef struct shash_node_s
  * @shead: A pointer to the first element of the sorted linked list
  * @stail: A pointer to the last element of the sorted linked list
  */
-typedef struct shash_table_s
-{
-	unsigned long int size;
-	shash_node_t **array;
-	shash_node_t *shead;
-	shash_node_t *stail;
+typedef struct shash_table_s {
+  unsigned long int size;
+  shash_node_t **array;
+  shash_node_t *shead;
+  shash_node_t *stail;
 } shash_table_t;
 
 /**
@@ -50,11 +48,10 @@ typedef struct shash_table_s
  * @value: The value corresponding to a key
  * @next: A pointer to the next node of the List
  */
-typedef struct hash_node_s
-{
-	char *key;
-	char *value;
-	struct hash_node_s *next;
+typedef struct hash_node_s {
+  char *key;
+  char *value;
+  struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -65,10 +62,9 @@ typedef struct hash_node_s
  * Each cell of this array is a pointer to the first node of a linked list,
  * because we want our HashTable to use a Chaining collision handling
  */
-typedef struct hash_table_s
-{
-	unsigned long int size;
-	hash_node_t **array;
+typedef struct hash_table_s {
+  unsigned long int size;
+  hash_node_t **array;
 } hash_table_t;
 
 /* 0-hash_table_create.c */
@@ -83,7 +79,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size);
 /* 3-hash_table_set.c */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 int addnode(hash_node_t *node, hash_table_t *ht, unsigned long int idx,
-						const char *key, const char *value);
+            const char *key, const char *value);
 /* 4-hash_table_get.c */
 char *hash_table_get(const hash_table_t *ht, const char *key);
 
@@ -92,7 +88,7 @@ void hash_table_print(const hash_table_t *ht);
 
 /* 6-hash_table_delete.c */
 void hash_table_delete(hash_table_t *ht);
-void delete_hash_nodes_recursive(hash_node_t *node);
+
 /* 100-sorted_hash_table.c */
 shash_table_t *shash_table_create(unsigned long int size);
 shash_node_t *add_n_shash(shash_node_t **, const char *, const char *);
