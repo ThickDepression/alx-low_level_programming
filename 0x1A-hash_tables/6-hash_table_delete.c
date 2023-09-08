881976cca037fe4c3f56c13a9aa5e3472f4d12ca
ThickDepression <yami.onlyme@gmail.com>
2023-09-08 18:23:21 +0100

    fix betty for 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -5,25 +5,28 @@
  * hash_table_delete - Deletes a hash table and its elements.
  * @ht: A pointer to the hash table.
  */
-void hash_table_delete(hash_table_t *ht) {
-  hash_node_t *current, *temp;
-  unsigned long int i = 0;
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *current, *temp;
+	unsigned long int i = 0;
 
-  if (ht == NULL)
-    return;
+	if (ht == NULL)
+		return;
 
-  for (; i < ht->size; i++) {
-    current = ht->array[i];
-    while (current != NULL) {
-      temp = current;
-      current = current->next;
-      free(temp->key);
-      free(temp->value);
-      free(temp);
-    }
-    free(current);
-  }
+	for (; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current != NULL)
+		{
+			temp = current;
+			current = current->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+		}
+		free(current);
+	}
 
-  free(ht->array);
-  free(ht);
+	free(ht->array);
+	free(ht);
 }
