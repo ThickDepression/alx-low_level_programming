bb9b92c6b257925ff1c86fb2ac548a67a80bf06e
Yeeloman <yami.onlyme@gmail.com>
2023-12-17 00:41:57 +0100

    fix from V to v
    layhdi alx

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -31,6 +31,6 @@ int interpolation_search(int *array, size_t size, int value)
 			high = prob - 1;
 	}
 	prob = low + (high - low) * (value - array[low]) / (array[high] - array[low]);
-	printf("Value checked array[%d] is out of range\n", prob);
+	printf("value checked array[%d] is out of range\n", prob);
 	return (-1);
 }
